09c6fd6532f8cb2a2024830f009659a947fb41b8
NVolzhanskiy <adheredn@gmail.com>
2023-03-14 14:08:40 +0300

    second task

---
+++ task_2/DataTypes.java
@@ -0,0 +1,12 @@
+package task_2;
+
+public class DataTypes {
+    public static void main(String[] args){
+        // 1. создание переменной типа double
+        double number = 2148109.4241124;
+        System.out.printf("Произволное дробное число типа double %f;\n", number);
+        byte small_number;
+        small_number = 127;
+        System.out.printf("Максимально возможное целое число типа byte %d;\n", small_number);
+    }
+}
