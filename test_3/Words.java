63172c972cc4ec973590317c543bb4a083fa80fa
NVolzhanskiy <adheredn@gmail.com>
2023-04-12 08:21:52 +0300

    update third test

--- test_3/Words.java
+++ test_3/Words.java
@@ -6,17 +6,17 @@ public class Words {
 
     static String find(String[] words){
         int k = -1;
-        int count_s = words[0].length();
+        int count_c = 0;
         for(String word : words){
             int n = 0;
             char[] word_arr = word.toCharArray();
             for(int i = 0; i < word_arr.length; i++){
                 for (int j = i+1; j < word_arr.length; j++){
-                    if(word_arr[i] == word_arr[j]) n++;
+                    if(word_arr[i] != word_arr[j]) n++;
                 }
             }
-            if(n < count_s) {
-                count_s = n;
+            if(n > count_c) {
+                count_c = n;
                 k++;
             }
         }
